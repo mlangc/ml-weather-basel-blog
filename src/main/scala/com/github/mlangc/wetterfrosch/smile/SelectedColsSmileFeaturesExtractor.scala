d100564017c9a0ca875a715d94e362c60f7dd65e
Matthias Langer <m.langer798@gmail.com>
2018-10-01 20:27:54 +0200

    Tweak Smile feature extractor

--- src/main/scala/com/github/mlangc/wetterfrosch/smile/SelectedColsSmileFeaturesExtractor.scala
+++ src/main/scala/com/github/mlangc/wetterfrosch/smile/SelectedColsSmileFeaturesExtractor.scala
@@ -1,8 +1,19 @@
 package com.github.mlangc.wetterfrosch.smile
 
-class SelectedColsSmileFeaturesExtractor(cols: Set[String]) extends SmileFeaturesExtractor {
+class SelectedColsSmileFeaturesExtractor private (cols: Array[Set[String]]) extends SmileFeaturesExtractor {
+  def this(cols1: Set[String], colsn: Set[String]*) = {
+    this((cols1 +: colsn).toArray)
+  }
+
+
   override def toFeatures(seq: Seq[Map[String, Double]]): Array[Double] = {
-    DefaultSmileFeaturesExtractor.toFeatures(seq.map(_.filterKeys(cols)))
+    assert(seq.size == cols.size)
+
+    DefaultSmileFeaturesExtractor.toFeatures {
+      seq.zip(cols).map { case (row, currentCols) =>
+        row.filterKeys(currentCols)
+      }
+    }
   }
 
   override def toFeaturesWithLabels(seqs: Seq[Seq[Map[String, Double]]], targetCol: String): (Array[Array[Double]], Array[Double]) = {
@@ -10,7 +21,7 @@ class SelectedColsSmileFeaturesExtractor(cols: Set[String]) extends SmileFeature
       val last = seq.size - 1
       seq.zipWithIndex.map { case (row, i) =>
           if (i == last) row
-          else row.filterKeys(cols)
+          else row.filterKeys(cols(i))
       }
     }
 
--- src/test/scala/com/github/mlangc/wetterfrosch/smile/SelectedColsSmileFeaturesExtractorTest.scala
+++ src/test/scala/com/github/mlangc/wetterfrosch/smile/SelectedColsSmileFeaturesExtractorTest.scala
@@ -1,5 +1,6 @@
 package com.github.mlangc.wetterfrosch.smile
 
+import com.github.mlangc.wetterfrosch.{HistoryExportColSubsets, HistoryExportCols}
 import com.github.mlangc.wetterfrosch.HistoryExportColSubsets.ColsFromLastDayForTree23
 import com.github.mlangc.wetterfrosch.HistoryExportCols._
 import org.scalatest.FreeSpec
@@ -7,25 +8,65 @@ import org.scalatest.FreeSpec
 class SelectedColsSmileFeaturesExtractorTest extends FreeSpec with SmileLabModule {
   "with daily data" - {
     val n = 100
-    lazy val labeledData = labeledDataAssembler.assemblyDailyData(1).take(n)
+    "going back one day" - {
+      lazy val labeledData = labeledDataAssembler.assemblyDailyData(1).take(n)
 
-    "with two features" in {
-      val cols = Set(SnowfallAmountDaily, MeanSeaLevelPressureDailyMean)
-      val featuresExtractor = new SelectedColsSmileFeaturesExtractor(cols)
-      val (features, labels) = featuresExtractor.toFeaturesWithLabels(labeledData, targetCol)
+      "with two features" in {
+        val cols = Set(SnowfallAmountDaily, MeanSeaLevelPressureDailyMean)
+        val featuresExtractor = new SelectedColsSmileFeaturesExtractor(cols)
+        checkedToFeaturesWithLabels(featuresExtractor, labeledData)
+      }
 
-      assert(features.size == n)
-      assert(labels.size == n)
-      assert(features.head.size == cols.size)
-      assert(features.toSeq.map(_.toSeq) == labeledData.map(seq => featuresExtractor.toFeatures(seq.init).toSeq))
-    }
-
-    "with predefined subsets" - {
-      "tree23" in {
-        val featuresExtractor = new SelectedColsSmileFeaturesExtractor(ColsFromLastDayForTree23)
-        val (features, _) = featuresExtractor.toFeaturesWithLabels(labeledData, targetCol)
-        assert(features.forall(_.size == ColsFromLastDayForTree23.size))
+      "with predefined subsets" - {
+        "tree23" in {
+          val featuresExtractor = new SelectedColsSmileFeaturesExtractor(ColsFromLastDayForTree23)
+          val features = checkedToFeaturesWithLabels(featuresExtractor, labeledData)
+          assert(features.forall(_.size == ColsFromLastDayForTree23.size))
+        }
       }
     }
+
+    "going back 3 days" - {
+      lazy val labeledData = labeledDataAssembler.assemblyDailyData(3).take(n)
+
+      "taking only data from the last day" in {
+        val featuresExtractor = new SelectedColsSmileFeaturesExtractor(Set(), Set(), ColsFromLastDayForTree23)
+        val features = checkedToFeaturesWithLabels(featuresExtractor, labeledData)
+        assert(features.head.size == ColsFromLastDayForTree23.size)
+      }
+
+      "taking a single column from every day" in {
+        val featuresExtractor = new SelectedColsSmileFeaturesExtractor(
+          Set(TotalPrecipitationDailySum), Set(MeanSeaLevelPressureDailyMin), Set(HighCloudCoverDailyMean))
+
+        val features = checkedToFeaturesWithLabels(featuresExtractor, labeledData)
+        assert(features.head.size == 3)
+        assert(features.head(0) == labeledData.head(0)(TotalPrecipitationDailySum))
+        assert(features.head(1) == labeledData.head(1)(MeanSeaLevelPressureDailyMin))
+        assert(features.head(2) == labeledData.head(2)(HighCloudCoverDailyMean))
+      }
+
+      "taking one column from the third day and two columns from the last day" in {
+        val featuresExtractor = new SelectedColsSmileFeaturesExtractor(
+          Set(TotalPrecipitationDailySum), Set(), Set(HighCloudCoverDailyMean, TotalCloudCoverDailyMean))
+
+        val features = checkedToFeaturesWithLabels(featuresExtractor, labeledData)
+        assert(features.head.size == 3)
+        assert(features.last(0) == labeledData.last(0)(TotalPrecipitationDailySum))
+        assert(features.last(1) == labeledData.last(2)(HighCloudCoverDailyMean))
+        assert(features.last(2) == labeledData.last(2)(TotalCloudCoverDailyMean))
+      }
+    }
+  }
+
+  private def checkedToFeaturesWithLabels(featuresExtractor: SmileFeaturesExtractor,
+                                          labeledData: Seq[Seq[Map[String, Double]]]): Array[Array[Double]] = {
+
+    val (features, labels) = featuresExtractor.toFeaturesWithLabels(labeledData, targetCol)
+    assert(features.size == labeledData.size)
+    assert(labels.size == features.size)
+    assert(features.toSeq.map(_.toSeq) == labeledData.map(seq => featuresExtractor.toFeatures(seq.init).toSeq))
+    assert(features.map(_.size).toSet.size == 1)
+    features
   }
 }
