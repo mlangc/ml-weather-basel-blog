0a14773ae7b0dc1491ebcf20ed669043586b32de
Matthias Langer <m.langer798@gmail.com>
2019-03-01 08:31:11 +0100

    Make CSV parser compatible with Java 11
    Also, ignore everything before the CSV header.

--- src/main/scala/com/github/mlangc/wetterfrosch/HistoryExportParser.scala
+++ src/main/scala/com/github/mlangc/wetterfrosch/HistoryExportParser.scala
@@ -2,18 +2,34 @@ package com.github.mlangc.wetterfrosch
 
 import java.net.URL
 
-import org.apache.commons.io.IOUtils
+import scala.io.Source
 
 class HistoryExportParser {
   def parse(url: URL): Seq[Map[String, Double]] = {
-    val lines = IOUtils.toString(url, "UTF-8").lines.toSeq
-    val headerNames = lines.head.split(";").map(_.trim)
-    val expectCols = headerNames.length
+    val source = Source.fromURL(url, "UTF-8")
+    try {
+      val lines = dropPreamble(source.getLines().toSeq)
+      val headerNames = splitLine(lines.head)
+      val expectCols = headerNames.length
 
-    lines.tail.zipWithIndex.map { case (line, lineNr) =>
-      val cells = line.split(";").map(_.trim)
-      assert(cells.length == expectCols, s"Line ${lineNr+1}: Expected $expectCols but got ${cells.length}")
-      headerNames.zip(cells).toMap.mapValues(_.toDouble)
+      lines.tail.zipWithIndex.map { case (line, lineNr) =>
+        val cells = line.split(";").map(_.trim)
+        assert(cells.length == expectCols, s"Line ${lineNr+1}: Expected $expectCols but got ${cells.length}")
+        headerNames.zip(cells).toMap.mapValues(_.toDouble)
+      }.toArray.toSeq
+    } finally {
+      source.close()
     }
+  }
+
+  private def dropPreamble(lines: Seq[String]): Seq[String] = {
+    lines.dropWhile { line =>
+      val cells = splitLine(line)
+      cells.exists(_.isEmpty)
+    }
+  }
+
+  private def splitLine(line: String): Array[String] = {
+    line.split(";").map(_.trim)
   }
 }
